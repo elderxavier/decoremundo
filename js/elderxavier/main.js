030928350ae03c3d2fde40e311a69e8803c8bfb4
Elder Xavier <eldersxavier@gmail.com>
2016-06-12 00:15:34 -0300

    Alterado Tema

---
+++ js/elderxavier/main.js
@@ -0,0 +1,41 @@
+jQuery(function ($) {
+    var PAGE = window.PAGE || {};
+    PAGE.mobilleMenu = function () {
+        $('#navbar-inner .fa-bars').click(function (e) {
+            $('.btn.btn-navbar').trigger('click');
+            e.preventDefault();
+        });
+    };
+    PAGE.header = function () {
+        $(document).scroll(function () {
+            if ($(window).scrollTop() > 42) {
+                $('.ma-header-container').addClass('activate');
+            } else {
+                $('.ma-header-container').removeClass('activate');
+            }
+        });
+     };
+
+
+    PAGE.preLoader = function () {
+        $(window).load(function () {
+        });
+    };
+
+    PAGE.posLoader = function () {
+        $(document).ready(function () {
+            PAGE.mobilleMenu();
+            PAGE.header();
+        });
+    };
+
+    PAGE.init = function () {
+        PAGE.posLoader();
+        PAGE.preLoader();
+
+    };
+
+    PAGE.init();
+});
+
+
